55957eb5ffd57b2e969223ebfd823bf0836c282c
ishay320 <ishay320@hotmail.com>
2025-02-10 21:30:51 +0200

    Added breaks light and separated to functions

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ struct car {
     Vector2 position;  // middle of the car
     float rotation;    // in degrees
 
+    bool breaks;
+
     // forces
     float mass;
     float force;
@@ -33,22 +35,55 @@ Vector2 Vector2Create(float direction, float power)
 }
 
 float force_to_acc(float force, float mass) { return force / mass; }
+
 float acc_to_velocity(float acceleration, double time_diff)
 {
     return acceleration * time_diff;
 }
 
-int main(int argc, char const **argv)
+void calculate_forces(struct car* car, double time_diff)
+{
+    car->acceleration = force_to_acc(car->force, car->mass);
+    car->velocity += acc_to_velocity(car->acceleration, time_diff);
+    car->position =
+        Vector2Add(car->position, Vector2Create(car->rotation, car->velocity));
+}
+
+void draw_car(const struct car car, const Vector2 origin)
+{
+    float length    = sqrtf(powf(car.length, 2) + powf(car.width, 2)) / 2;
+    float angle     = degree(tanhf(car.width / car.length));
+    Vector2 pos_fix = Vector2Create(car.rotation + angle, length);
+    DrawRectanglePro(
+        (Rectangle){car.position.x - pos_fix.x, car.position.y - pos_fix.y,
+                    car.length, car.width},
+        origin, car.rotation, car.color);
+    if (car.breaks) {
+        Vector2 pos_fix2 = Vector2Create(car.rotation + 90, car.width / 3 * 2);
+        DrawRectanglePro((Rectangle){car.position.x - pos_fix.x + pos_fix2.x,
+                                     car.position.y - pos_fix.y + pos_fix2.y,
+                                     car.length / 15, car.width / 3},
+                         origin, car.rotation, RED);
+        DrawRectanglePro(
+            (Rectangle){car.position.x - pos_fix.x, car.position.y - pos_fix.y,
+                        car.length / 15, car.width / 3},
+            origin, car.rotation, RED);
+    }
+    DrawCircle(car.position.x, car.position.y, 4, (Color){0, 0, 0, 255});
+}
+
+int main(int argc, char const** argv)
 {
     InitWindow(800, 450, "cars sim");
     SetTargetFPS(60);
     double time = GetTime();
 
-    struct car car = {.color        = {255, 0, 0, 255},
+    struct car car = {.color        = GREEN,
                       .width        = 30,
                       .length       = 50,
                       .position     = {50, 50},
                       .rotation     = 0,
+                      .breaks       = false,
                       .mass         = 20,
                       .force        = 0,
                       .acceleration = 0,
@@ -69,15 +104,7 @@ int main(int argc, char const **argv)
         ClearBackground(RAYWHITE);
         DrawFPS(10, 10);
         {
-            float length = sqrtf(powf(car.length, 2) + powf(car.width, 2)) / 2;
-            float angle  = degree(tanhf(car.width / car.length));
-            Vector2 pos_fix = Vector2Create(car.rotation + angle, length);
-            DrawRectanglePro(
-                (Rectangle){car.position.x - pos_fix.x,
-                            car.position.y - pos_fix.y, car.length, car.width},
-                origin, car.rotation, car.color);
-            DrawCircle(car.position.x, car.position.y, 4,
-                       (Color){0, 0, 0, 255});
+            draw_car(car, origin);
         }
         char text[255];
         sprintf(text, "rot: %f, x: %f, y: %f", car.rotation, last_force.x,
@@ -86,14 +113,19 @@ int main(int argc, char const **argv)
         EndDrawing();
 
         // logic
+        car.breaks = false;
         if (IsKeyDown(KEY_UP)) {
             car.force  = 100;
             last_force = Vector2Create(car.rotation, 1);
+            if (car.velocity < 0) {
+                car.breaks = true;
+            }
         }
         else if (IsKeyDown(KEY_DOWN)) {
             // TODO: make a better system
             if (car.velocity > 0) {  // breaks
-                car.force = -250;
+                car.force  = -250;
+                car.breaks = true;
             }
             else if (car.velocity > -100) {  // max speed in reverse
                 car.force = -50;
@@ -103,7 +135,8 @@ int main(int argc, char const **argv)
             }
         }
         else {
-            car.force = 0;
+            car.force  = 0;
+            car.breaks = false;
         }
 
         if (IsKeyDown(KEY_RIGHT)) {
@@ -115,10 +148,7 @@ int main(int argc, char const **argv)
 
         // TODO: add friction system
 
-        car.acceleration = force_to_acc(car.force, car.mass);
-        car.velocity += acc_to_velocity(car.acceleration, time_diff);
-        car.position =
-            Vector2Add(car.position, Vector2Create(car.rotation, car.velocity));
+        calculate_forces(&car, time_diff);
     }
 
     CloseWindow();
